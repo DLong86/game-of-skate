53400c7c1c161627d6da5b6eba318c037e72fab7
DLong86 <daveallenlong@gmail.com>
2024-02-15 16:27:33 +0900

    First commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="">Skate App</div>
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>WELCOME</h1>
+      <h3>Skateboarding App</h3>
+      <p>
+        App to help you warm-up with or to test your S-K-A-T-E Game against the
+        AI.
+      </p>
+    </div>
+  );
+}
